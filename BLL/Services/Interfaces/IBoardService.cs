873abf6798324c8b0f6bfe3fa66c05eef5facb8a
alexsardinin <alexsardinin@gmail.com>
2026-06-10 13:05:40 +0300

    Add BLL services (ColumnService, TaskService, etc.)

---
+++ BLL/Services/Interfaces/IBoardService.cs
@@ -0,0 +1,11 @@
+﻿namespace TrelloClone.BLL.Servises.Interfaces
+{
+    public interface IBoardService
+    {
+        Task<Board> CreateBoardAsync(Board board);
+        Task<Board?> GetBoardByIdAsync(int id);
+        Task<IEnumerable<Board>> GetBoardsByProjectIdAsync(int projectId);
+        Task<Board> UpdateBoardAsync(Board board);
+        Task<bool> DeleteBoardAsync(int id);
+    }
+}
